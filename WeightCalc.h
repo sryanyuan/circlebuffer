a91a9d611fd83bc287e34fa8bd6acda6bc185d40
sryanyuan <sryan@qq.com>
2016-12-15 16:01:34 +0800

    weight calculator

---
+++ WeightCalc.h
@@ -0,0 +1,36 @@
+#ifndef _INC_WEIGHTCALC_
+#define _INC_WEIGHTCALC_
+//////////////////////////////////////////////////////////////////////////
+#define DEFAULT_WEIGHTCALC_SIZE	32
+//////////////////////////////////////////////////////////////////////////
+#include <vector>
+//////////////////////////////////////////////////////////////////////////
+struct WeightCalcItem
+{
+	int nWeight;
+	int nValue;
+	// private fields
+	int nID;
+};
+typedef std::vector<WeightCalcItem> WeightCalcItemVector;
+//////////////////////////////////////////////////////////////////////////
+class WeightCalc
+{
+public:
+	WeightCalc();
+	~WeightCalc();
+
+public:
+	int AppendWeight(int _nWeight, int _nValue);
+	void RemoveWeight(int _nID);
+	void SetWeight(int _nID, int _nWeight);
+	void Reset();
+
+	int Get() const;
+
+private:
+	int m_nTotalWeight;
+	WeightCalcItemVector m_xItems;
+};
+//////////////////////////////////////////////////////////////////////////
+#endif
